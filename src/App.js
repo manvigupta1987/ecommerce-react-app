d12638e5c79262f02234ea6c6c7ec9283ccedd50
Manvi <manvigupta1987@gmail.com>
2020-10-02 09:36:39 -0400

    added collection preview component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import {Route} from 'react-router-dom'
+import Shop from './pages/shop/shop.component'
 
 class App extends Component {
   render() {
     return( 
       <div className='App'>
       <Route exact path='/' component ={HomePage} />
+      <Route exact path='/shop' component={Shop}></Route>
       </div>
     );
   }
