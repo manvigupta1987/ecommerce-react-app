f7372cb55e030084bf55d5cd004b335b501b17a0
Manvi <manvigupta1987@gmail.com>
2020-10-02 05:26:02 -0400

    added routing using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import {Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return( 
       <div className='App'>
-        <HomePage/>
+      <Route exact path='/' component ={HomePage} />
       </div>
     );
   }
 }
 
 export default App;
+
+
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'; // withRouter is the HOC component. 
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({section}) => {
+const MenuItem = ({section, history, match}) => {
   return (
-    <div className={`${section.size} menu-item`}>
+    <div className={`${section.size} menu-item`} onClick={()=>history.push(`${match.url}${section.title}`)}>
       <div style = {{ backgroundImage: `url(${section.imageUrl})`}} className='background-image'></div>
       <div className='content'>
         <h1 className='title'> {section.title.toUpperCase()}</h1>
@@ -13,4 +15,4 @@ const MenuItem = ({section}) => {
   );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem) ; //WithRouter will now return us the access to history, location, match props. 
