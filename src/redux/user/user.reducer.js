dacdab9c5b3bcb63c68efcb4e7aecd59ff95c212
Manvi <manvigupta1987@gmail.com>
2020-10-05 15:52:29 -0400

    added action type constant for redux

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,12 +1,14 @@
 
 // action is an object that contains two properties such as type and payload. Every reducer gets all the action fired by the view even if that 
 // action is not related to that reducer. 
+import UserActionType from './user.types'
+
 const INTIAL_STATE = {
   currentUser: null
 }
 const userReducer = (state=INTIAL_STATE, action) => {
   switch(action.type) {
-    case 'SET_CURRENT_USER' :
+    case UserActionType.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
