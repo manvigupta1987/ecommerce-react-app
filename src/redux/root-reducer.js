c9c9c9a8f50e3e0410f658ceee25b11e7ddd541e
Manvi <manvigupta1987@gmail.com>
2020-10-08 03:40:58 -0400

    redux-persist: local storage changes to keep the cart item when browser is closed or refresh

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,11 +7,24 @@ import { combineReducers } from  'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+//Redux-persist library changes.
+import {persistReducer} from 'redux-persist';
+// local storage object from redux-persist
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root', // at what point inside the reducer object, we want to store the data. 
+  storage, 
+  whitelist: ['cart'] // array of the reducer that we want to persist. 
+}
+
 // it returns the one big json state object where key is the value of the reducer. 
-export default combineReducers ({
+const rootReducer = combineReducers ({
   user: userReducer,
   cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
 
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+//redux-persist is the library used for including local storage or session storage in the application.
+import {persistStore} from 'redux-persist';
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+
+export const persistor = persistStore(store);
+export default {store, persistor };
