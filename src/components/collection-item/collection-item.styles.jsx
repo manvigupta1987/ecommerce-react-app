34873afce9b0a9e6e1f011b53982284504e2f1f3
Manvi <manvigupta1987@gmail.com>
2020-10-15 04:56:11 -0400

    bug fix button

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -8,38 +8,49 @@ export const CollectionItemContainer = styled.div`
   height: 350px;
   align-items: center;
   position: relative;
-
   &:hover {
     .image {
       opacity: 0.8;
     }
-    .custom-button {
+    button {
       opacity: 0.85;
       display: flex;
     }
   }
-  @media screen and (max-width:800px) {
+  @media screen and (max-width: 800px) {
     width: 40vw;
-
     &:hover {
-    .image {
-      opacity: unset;
-    }
-    .custom-button {
-      opacity: unset;
+      .image {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
     }
   }
+`;
 
+export const CustomButtonContainer = styled(CustomButton)`
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px;
   }
 `;
 
 export const ImageContainer = styled.div`
   width: 100%;
-    height: 95%;
-    background-size: cover;
-    background-position: center;
-    margin-bottom: 5px;
-    background-image: ${({imageUrl})=> `url(${imageUrl})`};
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 5px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
 
 export const CollectionFooter = styled.div`
@@ -53,30 +64,9 @@ export const CollectionFooter = styled.div`
 export const NameContainer = styled.span`
   width: 90%;
   margin-bottom: 15px;
-  @media screen and (max-width:800px) {
-    width: 85%;
-  }
 `;
 
 export const PriceContainer = styled.span`
   width: 10%;
   text-align: right;
-  @media screen and (max-width:800px) {
-    width: 15%;
-  }
-`;
-
-export const CustomButtonContainer = styled(CustomButton)`
-  width: 80%;
-  opacity: 0.7;
-  position: absolute;
-  top: 255px;
-  display: none;
-  
-  @media screen and (max-width:800px) {
-    display:block;
-    opacity: 0.9;
-    min-width: unset;
-    padding: 0 10px;
-  }
 `;
